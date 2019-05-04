be43091b0e7e84a957748d2e792e4609c840d397
homerrowsky <homerrowsky@yahoo.com.mx>
2019-05-04 12:53:45 -0500

    Ejercicio 2.3 - Vectores

---
+++ ex2.3.R
@@ -0,0 +1,4 @@
+# Crear y almacenar una secuencia de valores que vaya de 5 hasta 11 y progrese en saltos de 0.3
+# Con argumentos explicitos
+secuencia <- seq(from = 5, to = 11, by = 0.3)
+secuencia
